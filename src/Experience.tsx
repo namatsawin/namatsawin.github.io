daf4726619ef4fef2e20ff3a49eff9961f5e7ab2
Care Namatsawin <namatsawin.care@gmail.com>
2024-05-20 05:39:32 +0700

    update desc for exp

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -24,7 +24,7 @@ export function Experience({ isOpen, onClose }: Props) {
             a website called 'Foodstory Market' that allows restaurant owners to purchase ingredients to restock their inventory. 
             I also developed the web application 'Nigiwai Good Wealth' from scratch, a platform that supports franchisees in inventory restocking and franchise management.
             After the release of Nigiwai Good Wealth, the market team was disbanded due to business reasons, and I was transferred to the core team. There,
-            I undertook the task of revamping the web application 'Mobile Order,' a platform that allows customers visiting 
+            I undertook the task of entirely revamping the web application 'Mobile Order' a platform that allows customers visiting 
             a restaurant to place orders via the web application. However, it involved more than just UI revamping due to its legacy code. 
             So, I opted to rebuild it from the ground up using modern technologies to ensure higher quality, maintainability, and performance.
             Following this, the company merged with LINE MAN Wongnai, giving me the chance to work closely with their developers on 
