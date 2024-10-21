4093ed9eac70ee58bb8bcf13cb2df422dd995a86
Care Namatsawin <namatsawin.care@gmail.com>
2024-10-22 01:06:33 +0700

    hide porfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,141 @@
-import { useEffect, useState } from "react"
-import { motion } from "framer-motion"
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
-import Button from './components/Button'
-import GlowWrapper from './components/GlowWrapper'
-import FallingStar from "./components/FallingStar"
-import About from "./About"
-import Experience from "./Experience"
-import Portfolio from "./Portfolio"
-import LinkedInIcon from './assets/icons/linkedin.svg?react'
-import GithubIcon from './assets/icons/github.svg?react'
-import CallIcon from './assets/icons/call.svg?react'
-import EmailIcon from './assets/icons/email.svg?react'
+import Button from "./components/Button";
+import GlowWrapper from "./components/GlowWrapper";
+import FallingStar from "./components/FallingStar";
+import About from "./About";
+import Experience from "./Experience";
+import Portfolio from "./Portfolio";
+import LinkedInIcon from "./assets/icons/linkedin.svg?react";
+import GithubIcon from "./assets/icons/github.svg?react";
+import CallIcon from "./assets/icons/call.svg?react";
+import EmailIcon from "./assets/icons/email.svg?react";
 
-import HumanImage from './assets/images/human.webp'
-import ButtonAnchor from "./components/ButtonAnchor"
-import Resume from "./Resume"
+import HumanImage from "./assets/images/human.webp";
+import ButtonAnchor from "./components/ButtonAnchor";
+import Resume from "./Resume";
 
 function App() {
-  const urlSearchParams = new URLSearchParams(window.location.search).get('open')
+  const urlSearchParams = new URLSearchParams(window.location.search).get(
+    "open"
+  );
 
-  const [aboutModal, setAboutModal] = useState(false)
-  const [experienceModal, setExperienceModal] = useState(false)
-  const [portfolioModal, setPortfolioModal] = useState(false)
-  const [resumeModal, setResumeModal] = useState(false)
+  const [aboutModal, setAboutModal] = useState(false);
+  const [experienceModal, setExperienceModal] = useState(false);
+  const [portfolioModal, setPortfolioModal] = useState(false);
+  const [resumeModal, setResumeModal] = useState(false);
 
   useEffect(() => {
     switch (urlSearchParams?.toLocaleLowerCase()) {
-      case 'about': return setAboutModal(true)
-      case 'experience': return setExperienceModal(true)
-      case 'portfolio': return setPortfolioModal(true)
-      default: break;
+      case "about":
+        return setAboutModal(true);
+      case "experience":
+        return setExperienceModal(true);
+      case "portfolio":
+        return setPortfolioModal(true);
+      default:
+        break;
     }
-  }, [])
+  }, []);
 
   return (
     <motion.div className="p-10 rounded-xl max-w-[1366px] h-screen sm:h-full">
-      <motion.div 
+      <motion.div
         className="flex flex-col gap-10 relative mt-10"
         initial={{
           opacity: 0,
-          y: -25, 
-          x: -25
+          y: -25,
+          x: -25,
         }}
         animate={{
           opacity: 1,
-          y: 0,  
-          x: 0
+          y: 0,
+          x: 0,
         }}
         transition={{
-          delay: .6,
+          delay: 0.6,
           duration: 1,
-          ease: 'easeOut',
+          ease: "easeOut",
         }}
       >
-       <GlowWrapper className="mx-auto md:mx-0">
-          <h1 className="p-0 text-yellow-500 text-3xl font-bold text-center" style={{ textShadow: '1px 1px 2px #facc15'}}>
+        <GlowWrapper className="mx-auto md:mx-0">
+          <h1
+            className="p-0 text-yellow-500 text-3xl font-bold text-center"
+            style={{ textShadow: "1px 1px 2px #facc15" }}
+          >
             NAMATSAWIN BUNTARAWA
           </h1>
-       </GlowWrapper>
+        </GlowWrapper>
 
-       <div className="flex items-center justify-center gap-6 w-fit rounded-xl mx-auto md:mx-0 order-second md:order-last">
+        <div className="flex items-center justify-center gap-6 w-fit rounded-xl mx-auto md:mx-0 order-second md:order-last">
           <ButtonAnchor href="https://www.linkedin.com/in/namatsawin/">
-            <LinkedInIcon width={25} height={25} className="fill-[#0077b5]"/>
+            <LinkedInIcon width={25} height={25} className="fill-[#0077b5]" />
           </ButtonAnchor>
           <ButtonAnchor href="https://github.com/namatsawin">
-            <GithubIcon width={25} height={25} className="fill-neutral-100"/>
+            <GithubIcon width={25} height={25} className="fill-neutral-100" />
           </ButtonAnchor>
           <ButtonAnchor href="mailto:namatsawin.care@gmail.com">
-            <EmailIcon width={30} height={30} className="stroke-red-400"/>
+            <EmailIcon width={30} height={30} className="stroke-red-400" />
           </ButtonAnchor>
           <ButtonAnchor href="tel:+61421124443">
-            <CallIcon width={23} height={23} className="stroke-yellow-500"/>
+            <CallIcon width={23} height={23} className="stroke-yellow-500" />
             <span className="ml-1">AU</span>
           </ButtonAnchor>
           <ButtonAnchor href="tel:+66954791916">
-            <CallIcon width={23} height={23} className="stroke-yellow-500"/>
+            <CallIcon width={23} height={23} className="stroke-yellow-500" />
             <span className="ml-1">TH</span>
           </ButtonAnchor>
         </div>
 
         <p className="text-center md:max-w-[70%] md:text-left text-yellow-100 text-base">
-        I'm someone who puts my best effort into my work, making sure I do things right. I enjoy learning new things and facing challenges without backing down. Instead of being afraid of challenges, I see them as chances to grow. I take my duties seriously and make sure to finish what I start on time and at a high standard.
+          I'm someone who puts my best effort into my work, making sure I do
+          things right. I enjoy learning new things and facing challenges
+          without backing down. Instead of being afraid of challenges, I see
+          them as chances to grow. I take my duties seriously and make sure to
+          finish what I start on time and at a high standard.
         </p>
 
         <div className="grid md:flex gap-6 py-10">
-          <Button onClick={() => setAboutModal(!aboutModal)}>
-            About
-          </Button>
+          <Button onClick={() => setAboutModal(!aboutModal)}>About</Button>
           <Button onClick={() => setExperienceModal(!experienceModal)}>
             Experience
           </Button>
           <Button onClick={() => setPortfolioModal(!portfolioModal)}>
             Portfolio
           </Button>
-          <Button onClick={() => setResumeModal(!resumeModal)}>
-            Resume
-          </Button>
+          <Button onClick={() => setResumeModal(!resumeModal)}>Resume</Button>
         </div>
 
-      
-
-        <motion.img 
-          src={HumanImage} 
+        <motion.img
+          src={HumanImage}
           className="absolute right-[30px] bottom-0 opacity-30 border-none rounded-[50%] blur-lg sm:blur-md md:blur-sm lg:blur-none"
-          style={{ zIndex: '-1', boxShadow: '3.5rem 3rem 1rem 1rem #facc15'}}
+          style={{ zIndex: "-1", boxShadow: "3.5rem 3rem 1rem 1rem #facc15" }}
           width={500}
-          height={500} 
+          height={500}
           animate={{
             y: [0, -10, 0, 10, 0],
           }}
-          transition={{ duration: 5, repeat: Infinity, ease: 'easeInOut'  }}
+          transition={{ duration: 5, repeat: Infinity, ease: "easeInOut" }}
         />
       </motion.div>
-      <About isOpen={aboutModal} onClose={() => setAboutModal(false)}/>
-      <Experience isOpen={experienceModal} onClose={() => setExperienceModal(false)} />
-      <Portfolio isOpen={portfolioModal} onClose={() => setPortfolioModal(false)} />
+      <About isOpen={aboutModal} onClose={() => setAboutModal(false)} />
+      <Experience
+        isOpen={experienceModal}
+        onClose={() => setExperienceModal(false)}
+      />
+      {/* <Portfolio isOpen={portfolioModal} onClose={() => setPortfolioModal(false)} /> */}
       <Resume isOpen={resumeModal} onClose={() => setResumeModal(false)} />
 
-      <FallingStar/>
+      <FallingStar />
 
-      { import.meta.env.VITE_BUILD_AT && 
+      {import.meta.env.VITE_BUILD_AT && (
         <div className="fixed bottom-0 right-0 px-4 py-1 text-neutral-600 hidden md:block">
-        Build at {import.meta.env.VITE_BUILD_AT}
+          Build at {import.meta.env.VITE_BUILD_AT}
         </div>
-      }
+      )}
     </motion.div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ButtonAnchor.tsx
+++ src/components/ButtonAnchor.tsx
@@ -1,21 +1,24 @@
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
 interface Props extends React.PropsWithChildren {
-    href?: string
-    className?: string
+  href?: string;
+  className?: string;
 }
 
 export function ButtonAnchor({ children, href, className }: Props) {
-    return (
-        <motion.a 
-        className={`cursor-pointer ${className}`}
-        whileHover={{ scale: '1.1', transition: { duration: 1, ease: 'easeOut', }}}
-        href={href}
-        target="_blank"
-      >
-        {children}
-      </motion.a>
-    )
+  return (
+    <motion.a
+      className={`cursor-pointer ${className}`}
+      whileHover={{
+        scale: "1.1",
+        transition: { duration: 1, ease: "easeOut" },
+      }}
+      href={href}
+      target="_blank"
+    >
+      {children}
+    </motion.a>
+  );
 }
 
 export default ButtonAnchor;
