fa1bb36b2c1a328d799de4c79fa8aac988d59ab8
Care Namatsawin <66996968+namatsawin@users.noreply.github.com>
2025-02-23 20:49:37 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen">
       <motion.h1
         className="text-4xl font-bold text-center"
         variants={containerVariants}
